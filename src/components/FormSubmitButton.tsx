8c88b698d082fab99ed008bb29253d66769b09ac
Nikhil Yadav <facts.foundr@gmail.com>
2025-04-24 23:38:19 +0530

    Update FormSubmitButton.tsx

--- src/components/FormSubmitButton.tsx
+++ src/components/FormSubmitButton.tsx
@@ -6,7 +6,7 @@ import LoadingButton from "./LoadingButton";
 export default function FormSubmitButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>,
 ) {
-  const { pending } = useFormStatus();
+  const { pending }= useFormStatus();
 
   return <LoadingButton {...props} type="submit" loading={pending} />;
 }
